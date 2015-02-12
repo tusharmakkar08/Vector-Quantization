2f5b38a62a8d9951f4f87b6a9b1f30bdb156baa5
Tushar Makkar <tusharmakkar08@gmail.com>
2015-02-12 07:32:36 +0530

    Test Cases File made

--- test.py
+++ test.py
@@ -4,16 +4,25 @@ Author: Tushar Makkar <tusharmakkar08[at]gmail.com>
 Date: 12.02.2015
 '''
 
-def makeTestCases (numberOfFeatures, numberOfTestCases):
+import random
+
+def makeTestCases (rangeOfFeatures , numberOfFeatures, numberOfTestCases):
     '''
     Returns the matrix of Test Cases
     Args:
+        rangeOfFeatures : The numerical range of features [min, max]
+        numberOfFeatures : Total number of Features per feature vector
         numberOfTestCases : Total Number of Test Cases we want
     Returns:
         testCases : The Test Cases matrix matrix
     '''
     testCases = []
+    for testCase in xrange(numberOfTestCases):
+        tempTestCase = []
+        for feature in xrange(numberOfFeatures):
+            tempTestCase.append(random.randint(rangeOfFeatures[0], rangeOfFeatures[1]))
+        testCases.append(tempTestCase)
     return testCases
 
 if __name__ == '__main__':
-    makeTestCases(10)
+    print makeTestCases([1,5], 3, 4)
